f856207fb5b398a0f3157595c7a59a8ee3db0ed0
PrMax <89717747+PrMax@users.noreply.github.com>
2023-02-01 22:36:09 +0500

    Сделал связь между storyboards и ViewController. Так же прописал логику отображения на экране значении Slider. И при нажатии кнопки, высчитывается индекс веса.

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Label значений роста и веса
+    @IBOutlet weak var labelHeight: UILabel!
+    @IBOutlet weak var labelWeight: UILabel!
+    
+    //Слайдеры
+    @IBOutlet weak var sliderHieght: UISlider!
+    @IBOutlet weak var sliderWeight: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    //Action slider
+    @IBAction func changetSlaideHeight(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        labelHeight.text = "\(height)m"
+        
+    }
+    
+    //Action slider
+    @IBAction func changetSliderWeight(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        labelWeight.text = "\(weight)kg"
+    }
+    
+    
+    //Action Button
+    @IBAction func presserCalculate(_ sender: UIButton) {
+        //создаём константы со значенями
+        let height = sliderHieght.value
+        let weight = sliderWeight.value
+        //Вычисляем индекс веса
+        let bmi = weight / pow(height, 2)
+        
+    }
+    
 }
 
